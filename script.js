02dc06a98dc55768385b61c52e38498742e71e57
Sergey <sleptsoff3@gmail.com>
2022-05-28 18:41:59 +0300

    first version commit

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+console.log(16 + + ' 24');
+
+let incr = 10,
+    decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 == '8');
+
+&&
+||
+
+const isChecked = false,
+    isClose = false;
+
+console.log(!isChecked || isClose);
+
+console.log(2 + 2 * 2 !== '6');
