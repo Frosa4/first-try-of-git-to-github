fe0dd46dcb1683075b844b95a40b3f3f2acc3747
Sergey <sleptsoff3@gmail.com>
2022-05-29 16:31:52 +0300

    comparion lesson done

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
 'use strict';
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+let incr = 10,
+    decr = 10;
+const personalMovieDB = {
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
+    //++incr;
+    //--decr;
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('rate it', '');
+
+//     if (a != null && b != null && a!= '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('Done');
+//     } else {
+//         console.log('ne ne ne ne verno');
+//         // i--;
+//     }
 // }
 
-// do {
-//     console.log(num);
-//     num++;    
+// if (personalMovieDB.count < 10) {
+//     console.log('sliwkom malo uebok');
+// } else if(10 <= personalMovieDB.count <= 30) {
+//     console.log('normul');
+// } else if(personalMovieDB.count > 30) {
+//     console.log('ebat ti woobwe na ulice bivaesh?');
+// } else {
+//     console.log('TI POTS');
 // }
-// while (num <= 55);
 
-let num = 50;
+// console.log(personalMovieDB);
 
-for (let i = 1; i < 8; i++) {
-
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
-    
+if (personalMovieDB.count < 10) {
+    console.log('Sliwkom malo uebok');
+} else if(personalMovieDB >= 10 && personalMovieDB.count <30) {
+    console.log('serednychek');
+} else if(personalMovieDB.count >= 30) {
+    console.log('sidi lychwe viydi na ulitsu');
+} else {
+    console.log('Owibka naxui, vvodi davai cifri');
 }
