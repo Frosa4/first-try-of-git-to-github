b0c363b642104f92a9645c03866cdac3143da74a
Sergey <sleptsoff3@gmail.com>
2022-05-30 21:22:11 +0300

    cycles

--- script.js
+++ script.js
@@ -15,30 +15,21 @@ const personalMovieDB = {
     private: false
 };
 
-// for (let i = 0; i < 2; i++) {
-//     const a = prompt('Один из последних просмотренных фильмов?', ''),
-//         b = prompt('rate it', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('rate it', '');
 
-//     if (a != null && b != null && a!= '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('Done');
-//     } else {
-//         console.log('ne ne ne ne verno');
-//         // i--;
-//     }
-// }
+    if (a != null && b != null && a!= '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('ne ne ne ne verno');
+        // i--;
+    }
+}
 
-// if (personalMovieDB.count < 10) {
-//     console.log('sliwkom malo uebok');
-// } else if(10 <= personalMovieDB.count <= 30) {
-//     console.log('normul');
-// } else if(personalMovieDB.count > 30) {
-//     console.log('ebat ti woobwe na ulice bivaesh?');
-// } else {
-//     console.log('TI POTS');
-// }
 
-// console.log(personalMovieDB);
+console.log(personalMovieDB);
 
 if (personalMovieDB.count < 10) {
     console.log('Sliwkom malo uebok');
